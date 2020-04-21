9efbf4f1357d47ef6dae2fe236ea2c1e4a88cf52
ugur ciftci <ugurprog@gmail.com>
2020-04-22 00:48:41 +0300

    thread uygulaması yapıldı
    sıralı liste ve yarı yarıya kayıt eklemede problem var , thread başlatma ve bitirme işlemleri yapıldı

--- chapter-4-database-operations/src/main/java/questionThread/SeparateListSingleOrDouble.java
+++ chapter-4-database-operations/src/main/java/questionThread/SeparateListSingleOrDouble.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public class SeparateListSingleOrDouble implements Runnable {
     private List<Integer> tempArrayList;
-    private List<Integer> singleArrayList = new ArrayList<>();
-    private List<Integer> doubleArrayList = new ArrayList<>();
+    static List<Integer> singleArrayList = new ArrayList<>();
+    static List<Integer> doubleArrayList = new ArrayList<>();
+
+    public SeparateListSingleOrDouble(List<Integer> tempArrayList) {
+        this.tempArrayList = tempArrayList;
+    }
 
     @Override
     public void run() {
@@ -15,22 +19,18 @@ public class SeparateListSingleOrDouble implements Runnable {
         String threadName = Thread.currentThread().getName();
         System.out.println("My summation " + threadName + " is started!");
 
-
-        for (int i = 0; i < tempArrayList.size(); i++) {
-            System.out.println("Sayı alındı");
-            if (tempArrayList.get(i) % 2 == 0) {
-                addListNewElement(doubleArrayList, tempArrayList.get(i));
-                System.out.println(tempArrayList.get(i) + " -> sayısı çift");
-            } else {
-                addListNewElement(singleArrayList, tempArrayList.get(i));
-                System.out.println(tempArrayList.get(i) + " -> sayısı tek");
+        synchronized (this) {
+            for (int i = 0; i < tempArrayList.size(); i++) {
+                if (tempArrayList.get(i) % 2 == 0) {
+                    addListNewElement(doubleArrayList, tempArrayList.get(i));
+                    System.out.println(tempArrayList.get(i) + " -> sayısı çift");
+                } else {
+                    addListNewElement(singleArrayList, tempArrayList.get(i));
+                    System.out.println(tempArrayList.get(i) + " -> sayısı tek");
+                }
+                sleep(50);
             }
-            System.out.println("Sayı gönderildi -> " + tempArrayList.get(i));
-            sleep(50);
         }
-
-        System.out.println("teklerin sayısı : " + singleArrayList.size());
-        System.out.println("ciftlerin sayısı : " + doubleArrayList.size());
     }
 
     public static void sleep(long milliseconds) {
@@ -43,30 +43,6 @@ public class SeparateListSingleOrDouble implements Runnable {
 
     private synchronized void addListNewElement(List<Integer> addElementsList, Integer element) {
         addElementsList.add(element);
-    }
-
-    public static List<Integer> fillArrayList() {
-        List<Integer> tempArray = new ArrayList<>();
-        for (int i = 0; i < 10000; i++) {
-            tempArray.add(i + 1);
-        }
-        return tempArray;
-    }
-
-    public List<Integer> getSingleArrayList() {
-        return singleArrayList;
-    }
-
-    public void setSingleArrayList(List<Integer> singleArrayList) {
-        this.singleArrayList = singleArrayList;
-    }
-
-    public List<Integer> getDoubleArrayList() {
-        return doubleArrayList;
-    }
-
-    public void setDoubleArrayList(List<Integer> doubleArrayList) {
-        this.doubleArrayList = doubleArrayList;
     }
 
 
